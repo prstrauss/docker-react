fd59e380aec7feadf15c6b2fdb5c2c6bee1b8444
Strauss <strauss@rbot.fr>
2023-05-15 18:55:24 +0000

    update text App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Hello World !
+	  C'est un changement depuis la branche feature 
         </p>
         <a
           className="App-link"
